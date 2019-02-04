b2b79f47c1591fa343f305c85c2b601e6502cbf7
Jonathan Pepin <jypepin@gmail.com>
2019-02-04 18:28:53 +0100

    some cool progress

--- lib/avatar_web/views/page_view.ex
+++ lib/avatar_web/views/page_view.ex
@@ -1,6 +1,13 @@
 defmodule AvatarWeb.PageView do
   use AvatarWeb, :view
 
+  def all_originals do
+    Svg.originals_path
+    |> Enum.map(fn path ->
+      String.split(path, "static") |> Enum.at(1) 
+    end)
+  end
+
   def random_original do
     Svg.random_original
   end
