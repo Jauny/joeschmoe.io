07e027d138685b00fc4dbd98521badd0ffcd15af
Jonathan Pepin <jypepin@gmail.com>
2019-02-06 15:38:19 +0100

    update prod config for database

--- config/prod.exs
+++ config/prod.exs
@@ -16,7 +16,7 @@ config :avatar, AvatarWeb.Endpoint,
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
 
 # Configure your database
-config :hello, Hello.Repo,
+config :avatar, Avatar.Repo,
   url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
   ssl: true
