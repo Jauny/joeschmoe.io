da079de391ff733242dbb9905314e28ddde425d0
Jonathan Pepin <jypepin@gmail.com>
2019-02-06 15:50:13 +0100

    elixir 1.6.5

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Avatar.MixProject do
     [
       app: :avatar,
       version: "0.1.0",
-      elixir: "1.5.0",
+      elixir: "1.6.5",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
