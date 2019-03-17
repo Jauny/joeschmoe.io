3f9619166c3ca1ee8eb8fea428de6cd51f9a41ce
Jonathan Pepin <jypepin@gmail.com>
2019-03-17 14:47:08 +0100

    live view

--- lib/avatar_web/controllers/page_controller.ex
+++ lib/avatar_web/controllers/page_controller.ex
@@ -1,7 +1,13 @@
 defmodule AvatarWeb.PageController do
   use AvatarWeb, :controller
 
+  alias Phoenix.LiveView
+
   def index(conn, _params) do
     render(conn, "index.html")
+  end
+
+  def test_live(conn, _) do
+    LiveView.Controller.live_render(conn, AvatarWeb.TestLive, session: %{})
   end
 end
